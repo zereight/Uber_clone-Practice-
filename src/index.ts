41abe7e45e10a43a52bf2475cc6358ded0fb56da
zereight <aldwk68@naver.com>
2018-10-01 13:39:01 +0000

    2. setup Graphql App, setup index.ts( App starting with Options ) without schema

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+
+import app from "./app";
+
+import { Options } from "graphql-yoga";
+
+
+const PORT : number | string = process.env.PORT || 4000;
+const PLAYGROUND_ENDPOINT : string = "/playground";
+const GRAPHQL_ENDPOINT : string = "/graphql";
+
+const appOptions : Options = {
+    port: PORT,
+    playground: PLAYGROUND_ENDPOINT,
+    endpoint: GRAPHQL_ENDPOINT
+};
+
+const handling_start = ():void => {
+  console.log(`Start with Port : ${PORT}`);  
+};
+
+app.start( appOptions, handling_start );
