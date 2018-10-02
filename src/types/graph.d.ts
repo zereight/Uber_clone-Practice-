5ddff54448f8e22cc8104321368be64e82dff6a0
zereight <aldwk68@naver.com>
2018-10-02 14:23:11 +0000

    8. Make User Api(graphql), User Entity(User.ts)

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,16 +1,28 @@
-export const typeDefs = ["type Query {\n  sayBye(name: String!): Greeting!\n  sayHello: Greeting!\n}\n\ntype Greeting {\n  text: String!\n  error: Boolean!\n}\n"];
+export const typeDefs = ["type User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n  createAt: String!\n  updateAt: String\n}\n\ntype Query {\n  user: User!\n}\n"];
 /* tslint:disable */
 
 export interface Query {
-  sayBye: Greeting;
-  sayHello: Greeting;
+  user: User;
 }
 
-export interface SayByeQueryArgs {
-  name: string;
-}
-
-export interface Greeting {
-  text: string;
-  error: boolean;
+export interface User {
+  id: number;
+  email: string | null;
+  verifiedEmail: boolean;
+  firstName: string;
+  lastName: string;
+  age: number | null;
+  password: string | null;
+  phoneNumber: string | null;
+  verifiedPhoneNumber: boolean;
+  profilePhoto: string | null;
+  fullName: string | null;
+  isDriving: boolean;
+  isRiding: boolean;
+  isTaken: boolean;
+  lastLng: number | null;
+  lastLat: number | null;
+  lastOrientation: number | null;
+  createAt: string;
+  updateAt: string | null;
 }
